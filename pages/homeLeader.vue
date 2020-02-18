0dc7c3d61096c63ff409bd058624eaa24d4afb75
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-02-19 01:29:57 +0900

    Update homeLeader.vue

--- pages/homeLeader.vue
+++ pages/homeLeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="main">
+  <div class="rdmain">
     <engiiheader />
     <div class="blankl"></div>
     <div class="container">
       <img
         src="~assets/indeximg/homeLeaderremake.jpg"
-        width="60%"
+        width="70%"
         height="500px"
         alt
         class="img-fluid d-block mx-auto"
@@ -30,26 +30,27 @@
       </div>
 
       <div class="row mezasu-subtitle">
-        <div class="offset-3 col-6">
+        <div class="col-3"></div>
+        <div class="col-6">
           <p class="leadertext">
             「〇〇な人が知り合いにいたら」「もっと進学先を真剣に考えてたら」
-            <br>「学生時代にやっておけばよかった」「就活のときもっとしっかり考えるべきだった」「転職についてもっとリアルな声が聞きたい」等々、今までの人生の中で沢山の後悔と、いろんなものを望みました。
-            <br>学業にだらけ始めた高専3年次からは、1番大事な時期にも関わらず当時勤めていたアルバイトと部活だけに一生懸命になっていました。
-            <br>何の良さもない私でしたが、学校のブランドとバックアップのおかげもあり日本を代表するような企業に勤めることができました。その会社はいわゆる大手のホワイト企業で、福利厚生や将来性等、何をとってもピカイチでした。
-            <br>しかし、私のやりたいこと、やれること、やるべきこととは全く合わずとても苦しみました。
+            <br />「学生時代にやっておけばよかった」「就活のときもっとしっかり考えるべきだった」「転職についてもっとリアルな声が聞きたい」等々、今までの人生の中で沢山の後悔と、いろんなものを望みました。
+            <br />学業にだらけ始めた高専3年次からは、1番大事な時期にも関わらず当時勤めていたアルバイトと部活だけに一生懸命になっていました。
+            <br />何の良さもない私でしたが、学校のブランドとバックアップのおかげもあり日本を代表するような企業に勤めることができました。その会社はいわゆる大手のホワイト企業で、福利厚生や将来性等、何をとってもピカイチでした。
+            <br />しかし、私のやりたいこと、やれること、やるべきこととは全く合わずとても苦しみました。
             入社から3年目までは毎日のように冒頭に述べたようなタラレバを吐き出し、承認欲求に駆られ、毎日のように遊び歩き、沢山のものを失いました。しょうもない時間の使い方をしました。
-            <br>しかしそこから、少しずつ自らを変えるために努力をしました。正しい方向に、正しい姿勢で努力すると自分をとりまく色んな事が変わっていきました。
-            <br>そして今、私は毎日充実感のある生活をしています。今の私があるのは紛れもなく、沢山の人が力を貸してくれたおかげです。今の環境に身を置くべきか迷った時も本当に沢山の人が支えてくれました。みんなに助けてもらった分、今度は自らがに少しでも周りの人に自らの経験から得たことを発信して手助けできることはしたいと思いました。
-            <br>そこで、「失敗してもいいからやってみよう」と思い、知り合いのエンジニア達に協力してもらいこの【EnGii】をつくりました。
+            <br />しかしそこから、少しずつ自らを変えるために努力をしました。正しい方向に、正しい姿勢で努力すると自分をとりまく色んな事が変わっていきました。
+            <br />そして今、私は毎日充実感のある生活をしています。今の私があるのは紛れもなく、沢山の人が力を貸してくれたおかげです。今の環境に身を置くべきか迷った時も本当に沢山の人が支えてくれました。みんなに助けてもらった分、今度は自らがに少しでも周りの人に自らの経験から得たことを発信して手助けできることはしたいと思いました。
+            <br />そこで、「失敗してもいいからやってみよう」と思い、知り合いのエンジニア達に協力してもらいこの【EnGii】をつくりました。
             私達が持つものでどれだけの人にどれだけのことができるのかは全く分かりませんが
             やれるだけのことはやってみようと思います。
-            <br>みんなが後悔していること、悩んでいること、望むことの手助けをできたらなと思います。
+            <br />みんなが後悔していること、悩んでいること、望むことの手助けをできたらなと思います。
           </p>
           <div class="col-3"></div>
         </div>
       </div>
     </div>
-    <engiifooter class="leaderfooter"/>
+    <engiifooter class="leaderfooter" />
   </div>
 </template>
 
@@ -70,7 +71,11 @@ export default {
 
 <style>
 * {
-  font-family:  'Noto Sans JP', sans-serif;
+  font-family: "Noto Sans JP", sans-serif;
+}
+.rdmain {
+  margin-left: 0;
+  margin-right: 0;
 }
 .blankl {
   padding-top: 8%;
@@ -81,18 +86,20 @@ export default {
 .center {
   text-align: center;
 }
-.name-text{
-  padding-top:20px;
+.name-text {
+  padding-top: 20px;
 }
-.leader-title p {
-  font-size: 1.6vw;
+@media screen and (min-width: 767px) {
+  .chon p {
+    font-size: 1.3rem;
+    font-weight: bold;
+  }
 }
-.enlead p {
-  font-size: 1.3vw;
-}
-.chon p {
-  font-size: 1.9vw;
-  font-weight: bold;
+@media screen and (max-width: 767px) {
+  .chon p {
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
 }
 .mezasu-text {
   border-bottom: solid 1px;
@@ -100,17 +107,32 @@ export default {
 .name-text {
   text-align: center;
 }
-.mezasu-text p {
-  font-size: 1.3vw;
+@media screen and (min-width: 767px) {
+  .mezasu-text p {
+    font-size: 1.1rem;
+  }
 }
-.mezasu-subtitle{
+@media screen and (max-width: 767px) {
+  .mezasu-text p {
+    font-size: 0.7rem;
+  }
+}
+.mezasu-subtitle {
   padding-top: 20px;
 }
-.leadertext {
-  font-size: 1.3vw;
-  text-align: left;
+@media screen and (min-width: 767px) {
+  .leadertext {
+    font-size: 0.9rem;
+    text-align: left;
+  }
 }
-.leaderfooter{
+@media screen and (max-width: 767px) {
+  .leadertext {
+    font-size: 0.7rem;
+    text-align: left;
+  }
+}
+.leaderfooter {
   margin-top: 10px;
 }
 </style>
