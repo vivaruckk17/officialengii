5d859fcd8cf2998c1642b2657cee4a09e38531a4
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-02-20 15:59:08 +0900

    タイトルを変えた

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'EnGii',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
