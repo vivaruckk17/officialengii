97cf21a19d558802e745539492586cae3fcd1b28
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-02-24 14:23:43 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,36 +1,33 @@
 <template>
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-<input type="hidden" name="form-name" value="contact" />
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+  <form name="contact" method="POST" data-netlify="true" netlify>
+    <p>
+      <input type="hidden" name="form-name" value="contact" />
+      <label>
+        Your Name:
+        <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email:
+        <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Message:
+        <textarea name="message"></textarea>
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      isActive: false
-    };
-  },
-  methods: {
-    toggle_class: function() {
-      this.isActive = !this.isActive;
-    }
-  }
-};
+export default {};
 </script>
 
 <style>
-
 </style>
